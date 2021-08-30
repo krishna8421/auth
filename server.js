347c826fd9a3faa9dbde9e843a289ad0d059ecd1
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2021-08-30 22:52:41 +0530

    jwt and a bit html

--- server.js
+++ server.js
@@ -5,20 +5,48 @@ const login = require('./router/login')
 const port = process.env.PORT || 8421
 const bodyParser = require ('body-parser')
 const express = require('express');
+const path = require('path');
 const app = express()
+const jwt = require('jsonwebtoken')
 
 mongoConnect()
 
 app.use(bodyParser.json());
 app.use('/', signUp)
 app.use('/', login)
+app.use(express.static(path.join(__dirname, '/template/public')));
 
 
 app.get('/', (req, res) => {
-  res.send(`<center><h1>Welcome to the Api</h1></center>`);
+  res.sendFile(path.join(__dirname+'/template/index.html'));
 });
+
+
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req. headers['authorization' ];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ')
+    const bearerToken = bearer[1]
+    req.token = bearerToken
+    next()
+  }else{
+    res.status(422).json({ status: 'Login to See This URL'})
+  }
+
+}
+app.get('/private',verifyToken, (req, res) => {
+  jwt.verify(req.token,process.env.ACCESS_TOKEN, (err)=>{
+    if(err){
+      res.status(422)
+    }else{
+      res.sendFile(path.join(__dirname+'/template/private.html'));
+    }
+  })
+});
+
 
 // Start the app
 app.listen(port, () => {
     console.log(`Api started at Port:${port}`)
 });
+
